9228410c67b514852cffd12a09a1fb5037c83283
jon-rolfe <jon@rolfoid.com>
2015-11-25 22:47:28 -0500

    Yet more delinting! (This time: line breaks)

--- project.py
+++ project.py
@@ -57,8 +57,10 @@ def main(args):
     while(True):
         departdate = parse(
             raw_input('Form: Month Day Year (e.g. "December 8 2015")\n'), fuzzy=True)
-        if departdate < datetime.datetime.now() or departdate > (datetime.datetime.now() + datetime.timedelta(192)):
-            print 'You must enter a date in the future that is no more than 192 days from now.'
+        if departdate < datetime.datetime.now() or
+                departdate > (datetime.datetime.now() + datetime.timedelta(192)):
+            print ('You must enter a date in the future',
+                   'that is no more than 192 days from now.')
         else:
             correct = raw_input('OK, so you want to meet on %s? (Y/N)\n' %
                                 departdate.strftime('%A, %B %d, %Y'))
@@ -69,19 +71,24 @@ def main(args):
     while(True):
         returndate = parse(
             raw_input('Form: Month Day Year (e.g. "December 10 2015")\n'), fuzzy=True)
-        if returndate < datetime.datetime.now() or returndate > (datetime.datetime.now() + datetime.timedelta(192)) or returndate < departdate:
-            print 'You must enter a date after your departure that is no more than 192 days from now.'
+        if returndate < datetime.datetime.now() or
+                returndate > (datetime.datetime.now() + datetime.timedelta(192)) or
+                returndate < departdate:
+            print ('You must enter a date after your departure',
+                  'that is no more than 192 days from now.')
         else:
             correct = raw_input('OK, so you want to return on %s? (Y/N)\n' %
                                 returndate.strftime('%A, %B %d, %Y'))
             if correct.lower() == 'y':
                 break
     '''
+    # Debug only
     departdate = parse('december 8 2015')
     returndate = parse('december 10 2015')
     # clear terminal
     os.system('cls' if os.name == 'nt' else 'clear')
-    print 'Working on your trip between %s and %s, departing on %s and returning on %s.' % (origin_a, origin_b, departdate.strftime('%b %d, %Y'), returndate.strftime('%b %d, %Y'))
+    print ('Working on your trip between %s and %s, departing on %s and returning on %s.' % (
+        origin_a, origin_b, departdate.strftime('%b %d, %Y'), returndate.strftime('%b %d, %Y')))
     calculatemidpoint(origin_a, origin_b, departdate, returndate)
 
 
