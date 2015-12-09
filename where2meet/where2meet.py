fcfbeee00defcf6c17274be79db8ed7797d50b3b
jon-rolfe <jon@rolfoid.com>
2015-12-09 13:28:08 -0500

    further getargs tweaks

--- where2meet/where2meet.py
+++ where2meet/where2meet.py
@@ -28,36 +28,47 @@ def main():
     cli_parse.add_argument('second_origin', action='store',
                            help='the other origin city')
     cli_parse.add_argument('--no-autocorrect', action='store_true', dest='skip', default=False,
-                           help='skips autocorrection of origins; validates input against local DB')
+                           help='skips autocorrection and verification of origins; validates against local DB')
     cli_parse.add_argument('-d', '--departure', action='store',
                            required=False, dest='departdate', help='specify the departure date')
     cli_parse.add_argument('-r', '--return', action='store',
-                           required=False, dest='returndate', help='specify the return date')
+                           required=False, dest='returndate', help='specify the return date (requires -d)')
 
     args = cli_parse.parse_args()
+    departdate = returndate = None
 
     # somewhat confusingly, this parse() is the datetime parse (not
-    # argparse)
-    if args.departdate is not None or args.returndate is not None:
+    # argparse) - but parse depart/return dates if specified in cli
+
+    if args.departdate is not None:
         departdate = parse(args.departdate, fuzzy=True)
+        if args.departdate is (departdate < datetime.datetime.now() or
+                               departdate > (datetime.datetime.now() + datetime.timedelta(192))):
+            error('Invalid departure date specified.')
+    if (args.returndate and args.departdate) is not None:
         returndate = parse(args.returndate, fuzzy=True)
-        print departdate, returndate
+        if args.departdate is (departdate < datetime.datetime.now() or
+                               departdate > (datetime.datetime.now() + datetime.timedelta(192)) or
+                               departdate < departdate or (returndate - departdate) > datetime.timedelta(16)):
+            error('Invalid return date specified.')
 
     # clear terminal
-    # os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    # if they've disabled autocorrect, validate input against local DB
     if args.skip is True:
-        if validate(args.first_origin.upper()) == False or validate(args.second_origin.upper()) == False:
+        if validate((args.first_origin.upper()) or validate(args.second_origin.upper())) == False:
             error('Invalid origin or destination code specified.')
         else:
             origin_a = args.first_origin.upper()
             origin_b = args.second_origin.upper()
+    # otherwise, use SABRE autocorrect API to translate user input -> airport
     else:
         # translate names -> airports
         origin_a = suggest(args.first_origin)
         origin_b = suggest(args.second_origin)
 
-        if origin_a == False or origin_b == False:
+        if origin_a or origin_b == False:
             error('Invalid origin or destination specified.')
 
         print 'Assuming %s meant %s (id: %s).' % (args.first_origin, origin_a[0]['name'], origin_a[0]['id'])
@@ -67,9 +78,9 @@ def main():
         origin_a = origin_a[0]['id']
         origin_b = origin_b[0]['id']
 
-    # double check origins with user
+    # double check origins with user unless they've skipped checking
     print 'Origin A: %s\nOrigin B: %s' % (origin_a, origin_b)
-    while True:
+    while args.skip is False:
         correct = raw_input('Is this correct? (Y/N)\n')
         if correct.lower() == 'n':
             error('Please tweak your input and re-run the program.')
@@ -79,75 +90,76 @@ def main():
             print 'Invalid response.'
 
     # clear terminal
-    # os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     # figure out arrival/return dates of trip if not specified in CL args
-    print 'Please enter the date you would like to meet on.'
-    while True:
-        try:
-            departdate = parse(raw_input(
-                'Form: Month Day Year (e.g. "December 8 2015")\n'), fuzzy=True)
-        except:
-            print 'You must enter a valid date.'
-            continue
-        if (departdate < datetime.datetime.now() or
-                departdate > (datetime.datetime.now() + datetime.timedelta(192))):
-            print 'You must enter a date in the future', \
-                  'that is no more than 192 days from now.'
-        else:
-            correct = raw_input('OK, so you want to meet on %s? (Y/N)\n' %
-                                departdate.strftime('%A, %B %d, %Y'))
-            if correct.lower() == 'y':
-                break
-    # os.system('cls' if os.name == 'nt' else 'clear')
-    print 'Please enter the date you would like to return on.'
+    if departdate is None:
+        print 'Please enter the date you would like to meet on.'
+        while True:
+            try:
+                departdate = parse(raw_input(
+                    'Form: Month Day Year (e.g. "December 8 2015")\n'), fuzzy=True)
+            except:
+                print 'You must enter a valid date.'
+                continue
+            if (departdate < datetime.datetime.now() or
+                    departdate > (datetime.datetime.now() + datetime.timedelta(192))):
+                print 'You must enter a date in the future', \
+                      'that is no more than 192 days from now.'
+            else:
+                correct = raw_input('OK, so you want to meet on %s? (Y/N)\n' %
+                                    departdate.strftime('%A, %B %d, %Y'))
+                if correct.lower() == 'y' or '\n':
+                    break
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if returndate is None:
+        print 'Please enter the date you would like to return on.'
+        while True:
+            try:
+                returndate = parse(raw_input(
+                    'Form: Month Day Year (e.g. "December 10 2015")\n'), fuzzy=True)
+            except:
+                print 'You must enter a valid date.'
+                continue
+            if (returndate < datetime.datetime.now() or
+                    returndate > (datetime.datetime.now() + datetime.timedelta(192)) or
+                    returndate < departdate or (returndate - departdate) > datetime.timedelta(16)):
+                print 'You must enter a date after your departure', \
+                      'that is no more than\n192 days from now nor', \
+                      'more than 16 days past your departure date.'
+            else:
+                correct = raw_input('OK, so you want to return on %s? (Y/N)\n' %
+                                    returndate.strftime('%A, %B %d, %Y'))
+                if correct.lower() == 'y' or '\n':
+                    break
+    fullsearch(origin_a, departdate, returndate)
 
-    while True:
-        try:
-            returndate = parse(raw_input(
-                'Form: Month Day Year (e.g. "December 10 2015")\n'), fuzzy=True)
-        except:
-            print 'You must enter a valid date.'
-            continue
-        if (returndate < datetime.datetime.now() or
-                returndate > (datetime.datetime.now() + datetime.timedelta(192)) or
-                returndate < departdate or (returndate - departdate) > datetime.timedelta(16)):
-            print 'You must enter a date after your departure', \
-                  'that is no more than\n192 days from now nor', \
-                  'more than 16 days past your departure date.'
-        else:
-            correct = raw_input('OK, so you want to return on %s? (Y/N)\n' %
-                                returndate.strftime('%A, %B %d, %Y'))
-            if correct.lower() == 'y':
-                break
-
-    # Debug defaults
-    departdate = parse('december 8 2015')
-    returndate = parse('december 10 2015')
+    """
     # clear terminal
-    # os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print ('Working on your trip between %s and %s, departing on %s and returning on %s.' % (
         origin_a, origin_b, departdate.strftime('%b %d, %Y'), returndate.strftime('%b %d, %Y')))
     calculatemidpoint(origin_a, origin_b, departdate, returndate)
 
     print 'Top suggested destinations:'
-    movecursor()
-    threefares = nextthree()
+    movecursor('pricing')
+    threefares = printthree()
     if threefares == False:
         error('We were unable to find any midpoints for your specified airports.')
 
     while True:
         more = raw_input(
-            'Type "m" for more results. Type anything else to exit.\n')
-        if more == 'm':
-            # os.system('cls' if os.name == 'nt' else 'clear')
-            if nextthree() == False:
+            'Type enter for more results. Press CTRL + C or type q to quit.\n')
+        if more == 'q':
+            break
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            if printthree() == False:
                 print 'No more results.'
                 break
-        else:
-            break
 
     # Should be the end of access to DB, so close it
+    """
     closeandquit()
 
 
@@ -164,7 +176,7 @@ def calculatemidpoint(origin_a, origin_b, departdate, returndate):
     print 'Querying the server about %s.' % origin_b
     results_b = destinations(origin_b, departdate, returndate)
 
-    if results_a or results_b == False:
+    if (results_a or results_b) == False:
         error('One or both of the origins do not have scheduled air service.')
 
     print 'Calculating most balanced midpoint...'
